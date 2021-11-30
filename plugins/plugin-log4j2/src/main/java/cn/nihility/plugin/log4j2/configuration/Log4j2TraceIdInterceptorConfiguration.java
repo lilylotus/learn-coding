a3e9be782ef9664868d607063bc090ef98af6e41
lily <lily@nihility.cn>
2021-11-30 15:32:09 +0800

    log4j2

--- plugins/plugin-log4j2/src/main/java/cn/nihility/plugin/log4j2/configuration/Log4j2TraceIdInterceptorConfiguration.java
+++ plugins/plugin-log4j2/src/main/java/cn/nihility/plugin/log4j2/configuration/Log4j2TraceIdInterceptorConfiguration.java
@@ -1,9 +1,11 @@
 package cn.nihility.plugin.log4j2.configuration;
 
 import cn.nihility.plugin.log4j2.interceptor.Log4j2TraceIdInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@ConditionalOnClass(WebMvcConfigurer.class)
 public class Log4j2TraceIdInterceptorConfiguration implements WebMvcConfigurer {
 
     @Override
