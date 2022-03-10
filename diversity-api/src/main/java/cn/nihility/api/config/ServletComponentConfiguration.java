99622a7802794fff4af65e2fc626f4a39e79a5f3
lily <lily@nihility.cn>
2022-03-10 09:40:14 +0800

    feat(filter): servlet filter scan configuration

--- diversity-api/src/main/java/cn/nihility/api/config/ServletComponentConfiguration.java
+++ diversity-api/src/main/java/cn/nihility/api/config/ServletComponentConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ServletComponentScan(basePackages = {"cn.nihility.api.filter"})
+@ServletComponentScan(basePackages = {"cn.nihility.plugin.log4j2.filter", "cn.nihility.api.filter"})
 public class ServletComponentConfiguration {
 
 }
